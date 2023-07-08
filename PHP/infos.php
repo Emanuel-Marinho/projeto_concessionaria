bc2babdf17745c9100a5ec28f38bae22957c3de4
Emanuel Marinho <128088819+Emanuel-Marinho@users.noreply.github.com>
2023-07-08 10:49:56 -0300

    Add files via upload

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "concessionaria.ca6kf9jn1zqs.us-east-1.rds.amazonaws.com";
+$username = "emanuel";
+$password = "VxH4SwooUnfOtYa1lO81";
+$database = "db_conce";
+?>
